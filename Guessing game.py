f15ac9a68d10841b058d34e4cffcab223cd5af8f
HassemMD <hassemmohammed90@gmail.com>
2025-07-20 02:58:12 +0530

    added

--- Guessing game.py
+++ Guessing game.py
@@ -13,10 +13,10 @@ while guess != number:
        guess = int(input("a little LOWER! try again: "))
     else:
         guess = int(input("a little HIGHER! try again: "))
-    if guess_count ==1:
-        print(f"congratulations! you guessed the Correct number {number} in {guess_count} try!")
-    else:
-        print(f"congratulations! you guessed the Correct number {number} in {guess_count} tries!")
+if guess_count ==1:
+    print(f"congratulations! you guessed the Correct number '{number}' in {guess_count} try!")
+else:
+    print(f"congratulations! you guessed the Correct number '{number}' in {guess_count} tries!")
 print("THANKS FOR PLAYING !!!")
 
 
